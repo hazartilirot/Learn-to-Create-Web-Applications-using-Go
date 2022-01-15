b6e954d3957ffaf28fefac9c65736d810b6b42aa
hazartilirot <hazartilirot@gmail.com>
2022-01-15 12:22:29 +0200

    Implemented a basic routing

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -8,7 +8,12 @@ import (
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("There is a new page visitor")
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">"+
+			"support@lenslocked.com</a>")
+	}
 }
 
 func main() {
