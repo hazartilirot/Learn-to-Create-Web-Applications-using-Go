124724224dd2e98e94b14316b87c2b3ff157e46c
hazartilirot <hazartilirot@gmail.com>
2022-01-15 12:38:19 +0200

    Added a 404 page

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -13,6 +13,10 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">"+
 			"support@lenslocked.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you were looking for"+
+			"</h1><p>Please email us if you keep being sent to an invalid page</p>")
 	}
 }
 
