e785274b4d68102de1827a00b53391018c09f9ed
hazartilirot <hazartilirot@gmail.com>
2022-01-18 11:55:44 +0200

    Added in the schema package and used it to parse our forms into the SignupForm type

--- lenslocked.com/controllers/users.go
+++ lenslocked.com/controllers/users.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/gorilla/schema"
 	"github.com/username/project-name/views"
 	"net/http"
 )
@@ -22,7 +23,21 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	u.NewView.Render(w, nil)
 }
 
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 // Create processes the signup form when a user tries to create a new user account POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "This is a fake message. Pretend that we created the user account")
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	decoder := schema.NewDecoder()
+	var form SignupForm
+	if err := decoder.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 }
--- lenslocked.com/go.mod
+++ lenslocked.com/go.mod
@@ -2,4 +2,7 @@ module github.com/username/project-name
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
+)
