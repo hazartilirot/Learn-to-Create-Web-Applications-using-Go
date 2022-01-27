62196067da61a4790f6d589756bcdcaf602864da
hazartilirot <hazartilirot@gmail.com>
2022-01-27 18:50:19 +0100

    Created a rand package allowing us to create random strings and then store them as tokens

--- lenslocked.com/exp/main.go
+++ lenslocked.com/exp/main.go
@@ -2,51 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/username/project-name/models"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "root"
-	dbname   = "lenslocked_dev"
+	"github.com/username/project-name/rand"
 )
 
 func main() {
-	dsn := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	us, err := models.NewUserService(dsn)
-	if err != nil {
-		panic(err)
-	}
-	us.ResetDB()
-
-	user := models.User{
-		Name:  "John Doe",
-		Email: "john@example.com",
-	}
-
-	if err := us.Create(&user); err != nil {
-		panic(err)
-	}
-
-	user.Email = "johndoe@example.com"
-
-	if err = us.Update(&user); err != nil {
-		panic(err)
-	}
-
-	if err := us.Delete(user.ID); err != nil {
-		panic(err)
-	}
-
-	userByID, err := us.ByID(user.ID)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(userByID)
+	fmt.Println(rand.String(10))
+	fmt.Println(rand.RememberToken())
 }
