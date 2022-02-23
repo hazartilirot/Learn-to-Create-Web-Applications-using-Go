1b70a749ff64e9f0aabf65fc9b66400368dc8701
hazartilirot <hazartilirot@gmail.com>
2022-02-23 17:38:07 +0100

    Stopped requesting database for the current user's lookup

--- lenslocked.com/middleware/require_user.go
+++ lenslocked.com/middleware/require_user.go
@@ -4,6 +4,7 @@ import (
 	"github.com/username/project-name/context"
 	"github.com/username/project-name/models"
 	"net/http"
+	"strings"
 )
 
 type User struct {
@@ -16,6 +17,17 @@ func (mw *User) Apply(next http.Handler) http.HandlerFunc {
 
 func (mw *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		/*if the user is requesting a static asset or image we won't need to look up
+		the current user in the database. We skip the step*/
+		path := r.URL.Path
+
+		if strings.HasPrefix(path, "/assets/") ||
+			strings.HasPrefix(path, "/images/") {
+			next(w, r)
+			return
+		}
+
 		cookie, err := r.Cookie("remember_token")
 		if err != nil {
 			next(w, r)
