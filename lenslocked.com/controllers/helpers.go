fea8dcb3aff3cfb9666a8fab3be4f3c6cefb0ee9
hazartilirot <hazartilirot@gmail.com>
2022-04-02 00:35:44 +0200

    Implemented a form data persistent

--- lenslocked.com/controllers/helpers.go
+++ lenslocked.com/controllers/helpers.go
@@ -3,16 +3,28 @@ package controllers
 import (
 	"github.com/gorilla/schema"
 	"net/http"
+	"net/url"
 )
 
 func parseForm(r *http.Request, dst interface{}) error {
 	if err := r.ParseForm(); err != nil {
 		return err
 	}
+	return parseValues(r.PostForm, dst)
+}
+
+func parseURLParams(r *http.Request, dst interface{}) error {
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+	return parseValues(r.Form, dst)
+}
+
+func parseValues(values url.Values, dst interface{}) error {
 	decoder := schema.NewDecoder()
 	decoder.IgnoreUnknownKeys(true)
 
-	if err := decoder.Decode(dst, r.PostForm); err != nil {
+	if err := decoder.Decode(dst, values); err != nil {
 		return err
 	}
 	return nil
--- lenslocked.com/controllers/users.go
+++ lenslocked.com/controllers/users.go
@@ -28,7 +28,9 @@ type Users struct {
 }
 
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	u.NewView.Render(w, r, nil)
+	var form SignupForm
+	parseURLParams(r, &form)
+	u.NewView.Render(w, r, form)
 }
 
 type SignupForm struct {
@@ -41,6 +43,7 @@ type SignupForm struct {
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var vd views.Data
 	var form SignupForm
+	vd.Yield = &form
 	if err := parseForm(r, &form); err != nil {
 		vd.SetAlert(err)
 		u.NewView.Render(w, r, vd)
